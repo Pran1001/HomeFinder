dd87e279cb4e4695f78ca60ffa9518a1f416ca0a
Pran1001 <pranalidarekar99@gmail.com>
2021-12-08 20:19:39 +0530

    commit

--- HOMEFINDER/settings.py
+++ HOMEFINDER/settings.py
@@ -133,14 +133,13 @@ if DEBUG:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 
 MEDIA_URL = '/img/'
 
